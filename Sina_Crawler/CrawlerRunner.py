17759f3350a17715c7ff5396970de7a6a6383621
Mark-Fighting <msdfighting@163.com>
2015-04-02 22:47:35 +0800

    修改文本过滤条件

--- Sina_Crawler/CrawlerRunner.py
+++ Sina_Crawler/CrawlerRunner.py
@@ -147,10 +147,10 @@ class CrawlerRuner(object):
                 publish_time = info_map["publish_time"]
                 data_source["publish_time"] = datetime.datetime.strptime(publish_time,'%Y-%m-%d %H:%M:%S')
 
-            if sqlUtil.insert_data(data_source,self.table_name):
-                print u"插入完毕"
+            # if sqlUtil.insert_data(data_source,self.table_name):
+            #     print u"插入完毕"
 
-            print info_map
+            print data_source["info"]
             print data_source["publish_time"]
 
         except Exception,e:
